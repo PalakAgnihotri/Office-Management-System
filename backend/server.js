9b5c683b17062e2dedb76eae7193c192da4f39c1
Palak Agnihotri <143431220+PalakAgnihotri@users.noreply.github.com>
2026-02-21 21:34:03 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,26 +1,44 @@
-const express = require("express");
 const path = require("path");
+const express = require("express");
 const cors = require("cors");
 
+require("dotenv").config();
+const authRoutes = require("./routes/authRoutes");
+const taskRoutes = require("./routes/taskRoutes");
+const dashboardRoutes = require("./routes/dashboardRoutes");
+const courierRoutes = require("./routes/courierRoutes");
+const userRoutes = require("./routes/userRoutes");
+const notesRoutes = require("./routes/notesRoutes");
+const reportsRoutes = require("./routes/reportsRoutes");
+const chequeRoutes = require("./routes/chequeRoutes");
+const employeeRoutes=require("./routes/employeeRoutes");
 const app = express();
 
-app.use(cors());
+app.use(cors({origin:"*"}));
 app.use(express.json());
-
-// API routes
-app.use("/api/tasks", require("./routes/taskRoutes"));
-app.use("/api/auth", require("./routes/authRoutes"));
-
-// 🔥 Serve React build
-app.use(express.static(path.join(__dirname, "../frontend/build")));
-
-// Catch-all route (must be last)
-app.use((req, res) => {
-  res.sendFile(path.join(__dirname, "../frontend/build/index.html"));
+app.use("/api/tasks", taskRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/dashboard", dashboardRoutes);
+app.use("/api/courier", courierRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/notes", notesRoutes);
+app.use("/api/reports", reportsRoutes);
+app.use("/api/reports", reportsRoutes);
+app.use("/api/cheques", chequeRoutes);
+app.use("/api/employees",employeeRoutes);
+app.use("/uploads", express.static("uploads"));
+app.get("/", (req, res) => {
+  res.send("API Running...");
 });
 
 const PORT = process.env.PORT || 5000;
+// Serve frontend static files
+app.use(express.static(path.join(__dirname, "../frontend/build")));
 
+// Catch-all route (for React Router)
+app.use((req, res) => {
+  res.sendFile(path.join(__dirname, "../frontend/build/index.html"));
+});
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
