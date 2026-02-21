0b0236fef56d30404f8271f613024f3ffc7e4997
Palak Agnihotri <143431220+PalakAgnihotri@users.noreply.github.com>
2026-02-21 22:53:26 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,10 +1,16 @@
 const mysql = require("mysql2");
+
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT, 
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
+
 pool.getConnection()
   .then(conn => {
     console.log("✅ DATABASE CONNECTED");
@@ -13,3 +19,5 @@ pool.getConnection()
   .catch(err => {
     console.error("❌ DATABASE CONNECTION FAILED:", err);
   });
+
+module.exports = pool.promise();
