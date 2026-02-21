4ec59fa586a0b1e21e51b384dcb1170216f2cc36
Palak Agnihotri <agnihotripalak15@gmail.com>
2026-02-21 20:36:08 +0530

    Single deploy setup

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+const path = require("path");
 const express = require("express");
 const cors = require("cors");
 
@@ -31,6 +32,13 @@ app.get("/", (req, res) => {
 });
 
 const PORT = process.env.PORT || 5000;
+// Serve frontend static files
+app.use(express.static(path.join(__dirname, "../frontend/build")));
+
+// Catch-all route (for React Router)
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../frontend/build/index.html"));
+});
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
