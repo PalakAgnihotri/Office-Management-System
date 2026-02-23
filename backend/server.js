6324c922e03b580525554835ea3ba67c6cf74e9a
Palak Agnihotri <143431220+PalakAgnihotri@users.noreply.github.com>
2026-02-24 01:18:12 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -34,12 +34,12 @@ app.get("/", (req, res) => {
 
 const PORT = process.env.PORT || 5000;
 // Serve frontend static files
-// app.use(express.static(path.join(__dirname, "../frontend/build")));
+ app.use(express.static(path.join(__dirname, "../frontend/build")));
 
 // // Catch-all route (for React Router)
-// app.use((req, res) => {
-//   res.sendFile(path.join(__dirname, "../frontend/build/index.html"));
-// });
+app.use((req, res) => {
+  res.sendFile(path.join(__dirname, "../frontend/build/index.html"));
+ });
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
