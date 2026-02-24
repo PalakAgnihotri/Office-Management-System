c9c0a678ae0d52195d7cc8fd4ea0a3958037cfa5
Palak Agnihotri <143431220+PalakAgnihotri@users.noreply.github.com>
2026-02-24 12:12:42 +0530

    Update developmentRoutes.js

--- backend/routes/developmentRoutes.js
+++ backend/routes/developmentRoutes.js
@@ -6,5 +6,5 @@ const devController = require("../controllers/developmentController");
 router.post("/create", verifyToken, devController.createDevelopmentTask);
 router.get("/all", verifyToken, devController.getDevelopmentTasks);
 router.delete("/:id",verifyToken,devController.deleteDevelopmentTask);
-
+router.put("/:id",verifyToken, devController.updateDevelopmentTask);
 module.exports = router;
