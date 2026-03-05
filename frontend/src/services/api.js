45d20a29b116a4763de7184a5f70541d455336c0
Palak Agnihotri <143431220+PalakAgnihotri@users.noreply.github.com>
2026-03-06 03:09:04 +0530

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -3,7 +3,6 @@ import axios from "axios";
 const API = axios.create({
   baseURL: "https://office-management-system-1-0fwc.onrender.com/api"
 });
-
 API.interceptors.request.use((req) => {
   const token = localStorage.getItem("token");
   if (token) {
