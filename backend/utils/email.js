2c8af499fa2c69d3f0708859b74a48d2879a3c0c
Palak Agnihotri <143431220+PalakAgnihotri@users.noreply.github.com>
2026-03-06 12:17:12 +0530

    Update email.js

--- backend/utils/email.js
+++ backend/utils/email.js
@@ -7,7 +7,7 @@ const sendEmail = async (to, subject, text) => {
 
     console.log("📧 Sending email to:", to);
 
-    await resend.emails.send({
+    const response = await resend.emails.send({
       from: "Taskify <onboarding@resend.dev>",
       to: [to],
       subject: subject,
