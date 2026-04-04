632b1418dc431c7a616c1611a3c2b4ccf16b3903
Palak Agnihotri <143431220+PalakAgnihotri@users.noreply.github.com>
2026-04-04 21:55:43 +0530

    add

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -10,69 +10,69 @@ function Login() {
     email: "",
     password: ""
   });
-    const [loading, setLoading] = useState(false);
+  const [loading, setLoading] = useState(false);
   const [error, setError] = useState("");
 
   const handleLogin = async () => {
-  try {
-    setLoading(true);   // start loading
+    try {
+      setLoading(true);
       setError("");
-    const res = await API.post("/auth/login", form);
+      const res = await API.post("/auth/login", form);
 
-    console.log("LOGIN RESPONSE:", res.data);  
+      console.log("LOGIN RESPONSE:", res.data);
 
-    localStorage.setItem("token", res.data.token);
-    localStorage.setItem("role", res.data.user.role);
+      localStorage.setItem("token", res.data.token);
+      localStorage.setItem("role", res.data.user.role);
 
-    console.log("STORED ROLE:", res.data.user.role); 
+      console.log("STORED ROLE:", res.data.user.role);
 
-    if (res.data.user.role === "admin") {
-      navigate("/admin");
-    } else {
-      navigate("/employee");
+      if (res.data.user.role === "admin") {
+        navigate("/admin");
+      } else {
+        navigate("/employee");
+      }
+
+    } catch (err) {
+      alert("Invalid credentials");
+    } finally {
+      setLoading(false);
     }
 
-  } catch (err) {
-    alert("Invalid credentials");
-  } finally{
-    setLoading(false);
-  }
-
-};
+  };
 
   return (
     <AuthLayout title="Login">
 
       <input
         type="email"
-        placeholder="Email"
+        placeholder="admin@gmail.com/employee@gmail.com"
         className="w-full p-4 border border-gray-300 rounded-xl mb-6 focus:outline-none focus:ring-2 focus:ring-purple-500"
         onChange={(e) => setForm({ ...form, email: e.target.value })}
       />
 
       <input
         type="password"
-        placeholder="Password"
+        placeholder="123"
         className="w-full p-4 border border-gray-300 rounded-xl mb-8 focus:outline-none focus:ring-2 focus:ring-purple-500"
         onChange={(e) => setForm({ ...form, password: e.target.value })}
       />
 
       <button
-  onClick={handleLogin}
-  disabled={loading}
-  className="w-full py-4 rounded-full text-white font-semibold text-lg 
+        onClick={handleLogin}
+        disabled={loading}
+        className="w-full py-4 rounded-full text-white font-semibold text-lg 
   bg-gradient-to-r from-purple-500 to-pink-500 hover:opacity-90 transition
   flex items-center justify-center gap-2 disabled:opacity-70 disabled:cursor-not-allowed"
->
-  {loading ? (
-    <>
-      <div className="w-5 h-5 border-2 border-white border-t-transparent rounded-full animate-spin"></div>
-      Logging in...
-    </>
-  ) : (
-    "Login"
-  )}
-</button>
+      >
+        {loading ? (
+          <>
+            <div className="w-5 h-5 border-2 border-white border-t-transparent rounded-full animate-spin"></div>
+            Logging in...
+          </>
+        ) : (
+          "Login"
+        )}
+      </button>
     </AuthLayout>
   );
 }
